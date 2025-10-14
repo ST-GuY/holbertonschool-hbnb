798d2174dae24148fad3ab216a3601514da050dc
ST-GuY <estebanlvy23@gmail.com>
2025-10-14 11:00:42 +0200

    add place

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -3,13 +3,21 @@ from app.models import BaseModel
 
 
 class Place(BaseModel):
-    """
-    Modèle Place qui hérite de BaseModel.
-    Champs principaux : name, city, description
-    """
-    def __init__(self, name, city, description=None, **kwargs):
-        # Appelle le constructeur de BaseModel
-        super().__init__(**kwargs)
-        self.name = name
-        self.city = city
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()  # Initialise id, created_at, updated_at de BaseModel
+        self.title = title
         self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner  # Doit être une instance de User
+        self.reviews = []   # Liste pour stocker les Review liées
+        self.amenities = []  # Liste pour stocker les Amenity liés
+
+    def add_review(self, review):
+        """Ajoute un objet Review à la place"""
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        """Ajoute un objet Amenity à la place"""
+        self.amenities.append(amenity)
