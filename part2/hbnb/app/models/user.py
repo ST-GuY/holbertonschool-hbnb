ffc8a9a8d8acc1728980bd67e92ab58f35d372f7
ST-GuY <estebanlvy23@gmail.com>
2025-10-14 09:49:25 +0200

    user

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,12 +1,15 @@
+# app/models/user.py
 from app.models import BaseModel
+
 
 class User(BaseModel):
     """
-    Représente un utilisateur de la plateforme HBnB.
+    Modèle User qui hérite de BaseModel.
+    Champs principaux : first_name, last_name, email
     """
-    def __init__(self, **kwargs):
+    def __init__(self, first_name, email, last_name=None, **kwargs):
+        # Appelle le constructeur de BaseModel
         super().__init__(**kwargs)
-        self.first_name = kwargs.get("first_name", "")
-        self.last_name = kwargs.get("last_name", "")
-        self.email = kwargs.get("email", "")
-        self.password = kwargs.get("password", "")
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
