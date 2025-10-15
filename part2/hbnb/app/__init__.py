e1bbe557877094ab050327a2283d9f971e64fbc5
ST-GuY <estebanlvy23@gmail.com>
2025-10-15 09:20:04 +0200

    add create app

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,19 +1,12 @@
 from flask import Flask
 from flask_restx import Api
+from app.api.v1.users import api as users_ns
 
 
 def create_app():
-    # Créer l'application Flask
     app = Flask(__name__)
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
-    # Initialiser l'API RESTx
-    api = Api(
-        app,
-        version='1.0',
-        title='HBnB API',
-        description="HBnB Application API",
-        doc='/api/v1/'  # Chemin de la doc
-    )
-
-    # Retourner l'application
+    # Register the users namespace
+    api.add_namespace(users_ns, path='/api/v1/users')
     return app
