59b4c82f97d911d504c05c54fe6f1c807b15ff60
ST-GuY <estebanlvy23@gmail.com>
2025-11-17 14:54:14 +0100

    script

--- part4/scripts.js
+++ part4/scripts.js
@@ -1,8 +1,37 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
-  });
+  const loginForm = document.getElementById('login-form');
+
+  if (loginForm) {
+    loginForm.addEventListener('submit', async (event) => {
+      event.preventDefault(); // Empêche le comportement par défaut du formulaire
+
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+
+      // Appel API pour la connexion
+      try {
+        const response = await fetch('http://127.0.0.1:3000/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email, password })
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          // Stocker le token JWT dans un cookie
+          document.cookie = `token=${data.access_token}; path=/; Secure; SameSite=Strict`;
+          // Rediriger vers la page principale
+          window.location.href = 'index.html';
+        } else {
+          // Afficher erreur
+          alert('Échec de la connexion : vérifiez vos identifiants');
+        }
+      } catch (error) {
+        console.error('Erreur lors de la tentative de login:', error);
+        alert('Une erreur est survenue. Veuillez réessayer.');
+      }
+    });
+  }
+});
