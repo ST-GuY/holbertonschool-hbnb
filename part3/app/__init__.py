966f14865334c1521ef6623ac7b5d3da8464d9cd
ST-GuY <estebanlvy23@gmail.com>
2025-12-05 16:38:11 +0100

    bcrypt

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
 
+bcrypt = Bcrypt()
+
+
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    # Initialiser Bcrypt avec l'application
+    bcrypt.init_app(app)
+
+    # Créer l'API RESTx
     api = Api(
         app,
         version='1.0',
@@ -17,6 +25,7 @@ def create_app(config_class="config.DevelopmentConfig"):
         description='HBnB Application API'
     )
 
+    # Ajouter les namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
