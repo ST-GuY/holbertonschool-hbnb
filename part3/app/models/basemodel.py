6098c2a292024bc882f0c1a11b2069549afe4af8
ST-GuY <estebanlvy23@gmail.com>
2025-12-08 15:21:45 +0100

    indentation

--- part3/app/models/basemodel.py
+++ part3/app/models/basemodel.py
@@ -1,6 +1,7 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
@@ -17,12 +18,11 @@ class BaseModel:
             if hasattr(self, key):
                 setattr(self, key, value)
         self.save()  # Update the updated_at timestamp
-        
+
     def is_max_length(self, name, value, max_length):
         if len(value) > max_length:
             raise ValueError(f"{name} must be {max_length} characters max.") 
-        
-    
+
     def is_between(self, name, value, min, max):
         if not min < value < max:
             raise ValueError(f"{name} must be between {min} and {max}.")
