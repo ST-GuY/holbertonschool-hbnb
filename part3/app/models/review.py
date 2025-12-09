f09a3b7f170d7a485d4ca2dd062059f78da78614
ST-GuY <estebanlvy23@gmail.com>
2025-12-09 11:21:12 +0100

    mapping review

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -1,63 +1,50 @@
-from .basemodel import BaseModel
-from .place import Place
-from .user import User
+from app import db
 
-class Review(BaseModel):
-	def __init__(self, text, rating, place, user):
-		super().__init__()
-		self.text = text
-		self.rating = rating
-		self.place = place
-		self.user = user
-	
-	@property
-	def text(self):
-		return self.__text
-	
-	@text.setter
-	def text(self, value):
-		if not value:
-			raise ValueError("Text cannot be empty")
-		if not isinstance(value, str):
-			raise TypeError("Text must be a string")
-		self.__text = value
 
-	@property
-	def rating(self):
-		return self.__rating
-	
-	@rating.setter
-	def rating(self, value):
-		if not isinstance(value, int):
-			raise TypeError("Rating must be an integer")
-		super().is_between('Rating', value, 1, 6)
-		self.__rating = value
+class Review(db.Model):
+    __tablename__ = "reviews"
 
-	@property
-	def place(self):
-		return self.__place
-	
-	@place.setter
-	def place(self, value):
-		if not isinstance(value, Place):
-			raise TypeError("Place must be a place instance")
-		self.__place = value
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    place_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
 
-	@property
-	def user(self):
-		return self.__user
-	
-	@user.setter
-	def user(self, value):
-		if not isinstance(value, User):
-			raise TypeError("User must be a user instance")
-		self.__user = value
+    def __init__(self, text: str, rating: int, place_id: int = None, user_id: int = None):
+        self.text = text
+        self.rating = rating
+        self.place_id = place_id
+        self.user_id = user_id
 
-	def to_dict(self):
-		return {
-			'id': self.id,
-			'text': self.text,
-			'rating': self.rating,
-			'place_id': self.place.id,
-			'user_id': self.user.id
-		}
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, value):
+        if not value:
+            raise ValueError("Text cannot be empty")
+        if not isinstance(value, str):
+            raise TypeError("Text must be a string")
+        self._text = value
+
+    @property
+    def rating(self):
+        return self._rating
+
+    @rating.setter
+    def rating(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Rating must be an integer")
+        if not 1 <= value <= 5:
+            raise ValueError("Rating must be between 1 and 5")
+        self._rating = value
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'text': self.text,
+            'rating': self.rating,
+            'place_id': self.place_id,
+            'user_id': self.user_id
+        }
