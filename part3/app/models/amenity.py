e3d064ffc05fe65727127628052b99ab8489f723
ST-GuY <estebanlvy23@gmail.com>
2025-12-09 11:23:21 +0100

    mapping amenity

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -1,28 +1,31 @@
-from .basemodel import BaseModel
+from app import db
 
-class Amenity(BaseModel):
-	def __init__(self, name):
-		super().__init__()	
-		self.name = name
 
-	@property
-	def name(self):
-		return self.__name
+class Amenity(db.Model):
+    __tablename__ = "amenities"
 
-	@name.setter
-	def name(self, value):
-		if not isinstance(value, str):
-			raise TypeError("Name must be a string")
-		if not value:
-			raise ValueError("Name cannot be empty")
-		super().is_max_length('Name', value, 50)
-		self.__name = value
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
 
-	def update(self, data):
-		return super().update(data)
-	
-	def to_dict(self):
-		return {
-			'id': self.id,
-			'name': self.name
-		}
+    def __init__(self, name: str):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        if not value.strip():
+            raise ValueError("Name cannot be empty")
+        if len(value) > 50:
+            raise ValueError("Name cannot exceed 50 characters")
+        self._name = value
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
