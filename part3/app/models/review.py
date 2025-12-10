d8aa8d82ae0cc458bfce98a25ce462497c8ddbc1
ST-GuY <estebanlvy23@gmail.com>
2025-12-10 10:51:17 +0100

    relation

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -8,9 +8,17 @@ class Review(db.Model):
     text = db.Column(db.String(1024), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
 
+    # Relation Review → User (One-to-Many)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    # Relation Review → Place (One-to-Many)
+    place_id = db.Column(db.Integer, db.ForeignKey('places.id'), nullable=False)
+
     def to_dict(self):
         return {
             "id": self.id,
             "text": self.text,
-            "rating": self.rating
+            "rating": self.rating,
+            "user_id": self.user_id,
+            "place_id": self.place_id
         }
